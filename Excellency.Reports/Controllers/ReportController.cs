97853e85721fefd0339878680e4b100b57384d13
mendozaarjay <mendozaarjay.05@gmail.com>
2019-03-10 10:22:13 +0800

    Initial Commit

---
+++ Excellency.Reports/Controllers/ReportController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Excellency.Reports.Controllers
+{
+    public class ReportController : Controller
+    {
+        private string UserConnectionString =  ConfigurationManager.ConnectionStrings["UserConnectionString"].ConnectionString;
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #region KRA Report
+        public ActionResult PrintPerformanceAppraisal(int id)
+        {
+            DataSet ds = new DataSet();
+            ds.DataSetName = "dsAppraisal";
+            ds.Tables.Add(KRAHeader(id));
+            ds.Tables.Add(KRADetails(id));
+            var ReportParth = Server.MapPath("~/Reports/PerformanceAppraisal.rpt");
+            return new CrystalReportToPdf(ReportParth, ds);
+        }
+        private DataTable KRAHeader(int id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "[dbo].[spKRAReport]";
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@Id", id);
+            cmd.Parameters.AddWithValue("@QueryType", 0);
+            DataTable dt = SCObjects.ExecGetData(cmd, UserConnectionString);
+            dt.TableName = "Header";
+            return dt;
+        }
+        private DataTable KRADetails(int id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "[dbo].[spKRAReport]";
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@Id", id);
+            cmd.Parameters.AddWithValue("@QueryType", 1);
+            DataTable dt = SCObjects.ExecGetData(cmd, UserConnectionString);
+            dt.TableName = "KRA";
+            return dt;
+        }
+        #endregion
+        public ActionResult PrintBehavioral(int id)
+        {
+            DataSet ds = new DataSet();
+            ds.DataSetName = "dsAppraisal";
+            ds.Tables.Add(BehavioralHeader(id));
+            ds.Tables.Add(BehavioralDetails(id));
+            var ReportParth = Server.MapPath("~/Reports/Behavioral.rpt");
+            return new CrystalReportToPdf(ReportParth, ds);
+        }
+        private DataTable BehavioralHeader(int id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "[dbo].[spBehavioralReport]";
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@Id", id);
+            cmd.Parameters.AddWithValue("@QueryType", 0);
+            DataTable dt = SCObjects.ExecGetData(cmd, UserConnectionString);
+            dt.TableName = "Header";
+            return dt;
+        }
+        private DataTable BehavioralDetails(int id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "[dbo].[spBehavioralReport]";
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@Id", id);
+            cmd.Parameters.AddWithValue("@QueryType", 1);
+            DataTable dt = SCObjects.ExecGetData(cmd, UserConnectionString);
+            dt.TableName = "Behavioral";
+            return dt;
+        }
+    }
+}
---
+++ Excellency.Reports/SCObjects.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excellency.Reports
+{
+    public partial class SCObjects
+    {
+        /// <summary>
+        /// This method will return a scalar value based on the sql query/command text.
+        /// </summary>
+        /// <param name="SqlQuery">Sql Query/Command Text</param>
+        /// <param name="SqlConnectionString">Sql Connection String</param>
+        /// <returns></returns>
+        public static string ReturnText(string SqlQuery, string SqlConnectionString)
+        {
+            string _returnThis = string.Empty;
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+
+                cn.ConnectionString = SqlConnectionString;
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = SqlQuery;
+                _returnThis = cmd.ExecuteScalar().ToString();
+            }
+            catch (Exception ex)
+            {
+                _returnThis = string.Empty;
+            }
+            finally
+            {
+                if (cn.State == System.Data.ConnectionState.Open)
+                    cn.Close();
+            }
+            return _returnThis;
+        }
+        /// <summary>
+        /// This method will return a scalar value based on the sql query/command text asynchronously.
+        /// </summary>
+        /// <param name="SqlQuery">Sql Query/Command Text</param>
+        /// <param name="SqlConnectionString">Sql Connection String</param>
+        /// <returns></returns>
+        public static async Task<string> ReturnTextAsync(string SqlQuery, string SqlConnectionString)
+        {
+            string _returnThis = string.Empty;
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = SqlConnectionString;
+                await cn.OpenAsync();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = SqlQuery;
+                var a = await cmd.ExecuteScalarAsync();
+                _returnThis = a.ToString();
+            }
+            catch (Exception ex)
+            {
+                _returnThis = string.Empty;
+            }
+            finally
+            {
+                if (cn.State == System.Data.ConnectionState.Open)
+                    cn.Close();
+            }
+            return _returnThis;
+        }
+        /// <summary>
+        /// This method will return a datatable based on the sql query/command text.
+        /// </summary>
+        /// <param name="SqlQuery">Sql Query/Command Text</param>
+        /// <param name="SqlConnectionString">Sql Connection String</param>
+        /// <returns></returns>
+        public static DataTable LoadDataTable(string SqlQuery, string SqlConnectionString)
+        {
+            SqlConnection cn = new SqlConnection();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter();
+
+            try
+            {
+                cn.ConnectionString = SqlConnectionString;
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = SqlQuery;
+                da.SelectCommand = cmd;
+                da.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                dt = null;
+            }
+            finally
+            {
+                if (cn.State == System.Data.ConnectionState.Open)
+                    cn.Close();
+            }
+            return dt;
+        }
+        /// <summary>
+        /// This method will return a datatable based on the sql query/command text asynchronously.
+        /// </summary>
+        /// <param name="SqlQuery">Sql Query/Command Text</param>
+        /// <param name="SqlConnectionString">Sql Connection String</param>
+        /// <returns></returns>
+        public static async Task<DataTable> LoadDataTableAsync(string SqlQuery, string SqlConnectionString)
+        {
+            SqlConnection cn = new SqlConnection();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter();
+
+            try
+            {
+                cn.ConnectionString = SqlConnectionString;
+                await cn.OpenAsync();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = SqlQuery;
+                da.SelectCommand = cmd;
+                await Task.Run(() => da.Fill(dt));
+            }
+            catch (Exception ex)
+            {
+                dt = null;
+            }
+            finally
+            {
+                if (cn.State == System.Data.ConnectionState.Open)
+                    cn.Close();
+            }
+            return dt;
+        }
+        /// <summary>
+        /// This will return a string confirmation if the stored procedure was successfully executed.
+        /// </summary>
+        /// <param name="cmd">Sql Command</param>
+        /// <param name="SqlConnectionString">Sql Connection String</param>
+        /// <returns></returns>
+        public static string ExecuteNonQuery(SqlCommand cmd, string SqlConnectionString)
+        {
+            string _returnThis = string.Empty;
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = SqlConnectionString;
+            cn.Open();
+            SqlTransaction sqlTransaction = cn.BeginTransaction();
+            try
+            {
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Transaction = sqlTransaction;
+                cmd.ExecuteNonQuery();
+                sqlTransaction.Commit();
+                _returnThis = "Process has been successfully completed.";
+            }
+            catch (Exception ex)
+            {
+                _returnThis = "Errors.\n" + ex.Message;
+                sqlTransaction.Rollback();
+            }
+            finally
+            {
+                if (cn.State == ConnectionState.Open)
+                    cn.Close();
+            }
+            return _returnThis;
+        }
+        /// <summary>
+        /// This will return a string confirmation if the stored procedure was successfully executed asynchronously.
+        /// </summary>
+        /// <param name="cmd">Sql Command</param>
+        /// <param name="SqlConnectionString">Sql Connection String</param>
+        /// <returns></returns>
+        public static async Task<string> ExecuteNonQueryAsync(SqlCommand cmd, string SqlConnectionString)
+        {
+            string _returnThis = string.Empty;
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = SqlConnectionString;
+            await cn.OpenAsync();
+            SqlTransaction sqlTransaction = cn.BeginTransaction();
+            try
+            {
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Transaction = sqlTransaction;
+                var _checkThis = await cmd.ExecuteNonQueryAsync();
+                _returnThis = "Process has been successfully completed.";
+                sqlTransaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                _returnThis = "Errors.\n" + ex.Message;
+                sqlTransaction.Rollback();
+            }
+            finally
+            {
+                if (cn.State == ConnectionState.Open)
+                    cn.Close();
+            }
+            return _returnThis;
+        }
+        /// <summary>
+        /// This will return a string confirmation if the stored procedure was successfully executed.
+        /// </summary>
+        /// <param name="cmdList">List of Sql Command</param>
+        /// <param name="SqlConnectionString">Sql Connection String</param>
+        /// <returns></returns>
+        public static string ExecuteNonQuery(List<SqlCommand> cmdList, string SqlConnectionString)
+        {
+            string _returnThis = string.Empty;
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = SqlConnectionString;
+            cn.Open();
+            SqlTransaction sqlTransaction = cn.BeginTransaction();
+            try
+            {
+                foreach (SqlCommand cmd in cmdList)
+                {
+                    cmd.Connection = cn;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Transaction = sqlTransaction;
+                    cmd.ExecuteNonQuery();
+                }
+                _returnThis = "Process has been successfully completed.";
+                sqlTransaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                _returnThis = "Errors.\n" + ex.Message;
+                sqlTransaction.Rollback();
+            }
+            finally
+            {
+                if (cn.State == ConnectionState.Open)
+                    cn.Close();
+            }
+            return _returnThis;
+        }
+        /// <summary>
+        /// This will return a string confirmation if the stored procedure was successfully executed asynchronously.
+        /// </summary>
+        /// <param name="cmdList">List of Sql Command</param>
+        /// <param name="SqlConnectionString">Sql Connection String</param>
+        /// <returns></returns>
+        public static async Task<string> ExecuteNonQueryAsync(List<SqlCommand> cmdList, string SqlConnectionString)
+        {
+            string _returnThis = string.Empty;
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = SqlConnectionString;
+            await cn.OpenAsync();
+            SqlTransaction sqlTransaction = cn.BeginTransaction();
+            try
+            {
+                foreach (SqlCommand cmd in cmdList)
+                {
+                    cmd.Connection = cn;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Transaction = sqlTransaction;
+                    var _checkThis = await cmd.ExecuteNonQueryAsync();
+                }
+                _returnThis = "Process has been successfully completed.";
+                sqlTransaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                _returnThis = "Errors.\n" + ex.Message;
+                sqlTransaction.Rollback();
+            }
+            finally
+            {
+                if (cn.State == ConnectionState.Open)
+                    cn.Close();
+            }
+            return _returnThis;
+        }
+        /// <summary>
+        /// This will return a datatable based on the stored procedure.
+        /// </summary>
+        /// <param name="cmd">Sql Command</param>
+        /// <param name="SqlConnectionString">Sql Connection String</param>
+        /// <returns></returns>
+        public static DataTable ExecGetData(SqlCommand cmd, string SqlConnectionString)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = SqlConnectionString;
+            cn.Open();
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                da.SelectCommand = cmd;
+                da.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                dt = null;
+            }
+            finally
+            {
+                if (cn.State == ConnectionState.Open)
+                    cn.Close();
+            }
+            return dt;
+        }
+        /// <summary>
+        /// This will return a datatable based on the stored procedure asynchronously. 
+        /// </summary>
+        /// <param name="cmd">Sql Command</param>
+        /// <param name="SqlConnectionString">Sql Connection String</param>
+        /// <returns></returns>
+        public static async Task<DataTable> ExecGetDataAsync(SqlCommand cmd, string SqlConnectionString)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = SqlConnectionString;
+            await cn.OpenAsync();
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                da.SelectCommand = cmd;
+                await Task.Run(() => da.Fill(dt));
+            }
+            catch (Exception ex)
+            {
+                dt = null;
+            }
+            finally
+            {
+                if (cn.State == ConnectionState.Open)
+                    cn.Close();
+            }
+            return dt;
+        }
+    }
+}
